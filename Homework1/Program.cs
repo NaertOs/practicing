d9e62c3c91b2670ca0706ff00344352cbc7fc742
Evgeniy Usoltsev <lafabbiano@mail.ru>
2019-05-21 14:51:41 +0700

    1-2

---
+++ Homework1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            //Задание №1
+            Console.WriteLine("Приветствую! Введите свое имя");
+            string Name;
+            Name = Console.ReadLine();
+            Console.WriteLine("Введите свою фамилию");
+            string SecName;
+            SecName = Console.ReadLine();
+            Console.WriteLine("Добро пожаловать, "+SecName+" "+Name);
+            Console.ReadKey();
+            //Конец задания №1
+            //Задание №1 часть 2
+            string FName, SName;
+            FName = Interaction.InputBox("Введите свое имя", "Ввод данных");
+            SName = Interaction.InputBox("Введите свою фамилию", "Ввод данных");
+            MessageBox.Show("Добро пожаловать, "+SName+" "+FName+"!", "Welcome", MessageBoxButtons.OK,MessageBoxIcon.Information);    
+            //Задание №1 часть 2 конец
+            */
+            //-----------------------------------------------------------------Задание №2-------------------------------------------------
+            /*
+
+            Console.WriteLine("Приветствую! Введите свое имя");
+            string Name;
+            Name = Console.ReadLine();
+            Console.WriteLine("Введите свой возраст");
+            string age;
+            age = Console.ReadLine();
+            Console.WriteLine("Страдай, "+Name+", ибо тебе уже "+age);
+            Console.ReadKey();
+            //--------------------------------------------------------------Конец задания №2--------------------------------------------
+            //-------------------------------------------------------------Задание №2 часть 2---------------------------------------------
+            string FName, SName;
+            FName = Interaction.InputBox("Введите свое имя", "Ввод имени");
+            SName = Interaction.InputBox("Введите свой возраст", "Ввод возраста");
+            MessageBox.Show("Добро пожаловать, "+FName+", "+SName+"!", "Welcome", MessageBoxButtons.OK,MessageBoxIcon.Information);    
+            //----------------------------------------------------------Задание №2 часть 2 конец-------------------------------------------
+           // */
+        }
+    }
+}
